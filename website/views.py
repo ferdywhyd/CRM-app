b53b3f26a14e92ca8d859c643010bce315213484
Ferdy Wahyudi <ferdyw6@gmail.com>
2023-09-29 16:35:28 +0700

    update registrasi

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -25,4 +26,17 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticated dan Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user =  authenticate(username=username, password=password)
+            login(request,user)
+            messages.success(request, "Kamu Berhasil Registrasi")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {'form':form})

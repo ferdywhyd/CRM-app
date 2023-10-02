796df6d2b69388b2f866b8d9be310d8b6f5433aa
Ferdy Wahyudi <ferdyw6@gmail.com>
2023-10-03 00:23:22 +0700

    update record

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -49,6 +49,42 @@ def customer_record(request, pk):
     if request.user.is_authenticated:
         customer_record = Record.objects.get(id=pk)
         return render(request, "record.html", {'customer_record':customer_record})
+    else:
+        messages.success(request, "Kamu harus login brader")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_user = Record.objects.get(id=pk)
+        delete_user.delete()
+        messages.success(request, "Berhasil Dihapuss dari kenyataan")
+        return redirect('home')
+    else:
+        messages.success(request, "Kamu harus Login Braderrr")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Berhasil Ditambahkan")
+                return redirect('home')
+        return render(request, "add_record.html", {'form':form})
+    else:
+        messages.success(request, "Kamu harus login brader")
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Berhasil Ditambahkan")
+            return redirect('home')
+        return render(request, "update_record.html", {'form':form})
     else:
         messages.success(request, "Kamu harus login brader")
         return redirect('home')
